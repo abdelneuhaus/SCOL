78e1407d1a1936f8fdcb8daaf3ab22921b5c09f1
Abdelghani <abdelghani.neuhaus@u-bordeaux.fr>
2026-05-07 16:55:17 +0200

    adding function descriptions

--- SCOL/CARE/training.py
+++ SCOL/CARE/training.py
@@ -6,8 +6,25 @@ from csbdeep.models import Config, CARE
 
 
 
-def do_training(kernel_size=5, unet_depth=2, unet_first_layer=16, train_epochs=40,
-                lr=4e-4, steps_epochs=100, data_care="none.npz", save_in="here"):
+def do_training(kernel_size:int=5, unet_depth:int=2, unet_first_layer:int=64, train_epochs:int=40, 
+                lr=4e-4, steps_epochs:int=400, data_care:str="model.npz", save_in:str="current_directory"):
+    
+    """
+    Perform the training using 2D-CARE network.
+    Based on https://github.com/CSBDeep/CSBDeep code, specifically tutorial notebooks.
+
+    Args:
+        kernel_size (int): kernel size (generally 3x3 or 5x5). Must be odd.
+        unet_depth (int): U-Net depth.
+        unet_first_layer (int): number of layers in the first layer
+        train_epochs (int): number of epochs to train the network
+        lr (float): initial learning rate value.
+        steps_epochs (int): number of steps per epochs
+        data_care (str): path to NPZ file
+        save_in (str): path to save the trained model.
+    Returns:
+        None. Save the 2D-CARE model and display losses curves.
+    """
 
     (X,Y), (X_val,Y_val), axes = load_training_data(data_care, validation_split=0.1, verbose=True)
     c = axes_dict(axes)['Z']
@@ -18,14 +35,14 @@ def do_training(kernel_size=5, unet_depth=2, unet_first_layer=16, train_epochs=4
                     n_channel_out=1,
                     unet_kern_size=kernel_size,
                     unet_n_depth=unet_depth,
-                    unet_n_first=64,    # or 32
+                    unet_n_first=unet_first_layer,
                     train_batch_size=16,
                     # train_steps_per_epoch=100,  # 400 initially
                     train_epochs=train_epochs,
                     train_learning_rate=lr,
-                    unet_residual=False,
+                    unet_residual=True,
                     train_reduce_lr = {'factor':0.5, 'patience':10, 'min_delta':1e-4},
-                    train_loss = 'weighted_loss_tf',   # weighted_loss_tf
+                    train_loss = 'weighted_loss_tf',
                     )
 
     print(config)
