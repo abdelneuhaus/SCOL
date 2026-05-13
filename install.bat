b0bb3a281450d720c0474d328c407cffc864d5ad
Abdelghani Neuhaus <aneuhaus@MacBook-Pro-de-Abdelghani.local>
2026-05-13 16:06:48 +0200

    added aydin to SCOL

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ echo.
 echo [1/3] Creating CSBDeep environment...
 py -3.9 -m venv venv_csbdeep
 call venv_csbdeep\Scripts\activate.bat
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip setuptools wheel
 pip install -r .\requirements\csbdeep_requirements.txt
 call venv_csbdeep\Scripts\deactivate.bat
 
@@ -15,15 +15,16 @@ echo.
 echo [2/3] Creating Noise2Self environment...
 py -3.9 -m venv venv_aydin
 call venv_aydin\Scripts\activate.bat
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip setuptools wheel
 pip install -r .\requirements\aydin_requirements.txt
+pip install tensorflow==2.10.0
 call venv_aydin\Scripts\deactivate.bat
 
 echo.
 echo [3/3] Creating main environment...
 py -3.9 -m venv venv
 call venv\Scripts\activate.bat
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip setuptools wheel
 pip install -r .\requirements\project_requirements.txt
 call venv\Scripts\deactivate.bat
 
